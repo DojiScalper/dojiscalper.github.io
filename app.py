0093ddc526ca4381c0939064462bf3912db5b90c
Solid-Wires <solid.wires@gmail.com>
2024-08-14 11:51:02 -0400

    Inital Flask integration steps, although from looking it up myself, it does not seem like it is going to work with Github Pages.

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template # Only import flask from the Flask library
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
